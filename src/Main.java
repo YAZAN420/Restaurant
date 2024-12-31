ce8e479cef304934c7015c33e8182af370ffae65
loulia alshaar <you@example.com>
2024-12-31 23:26:45 +0300

    comment

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         ReportHandle.load2Map(Report.mealsCounter, "ReportMealsCounter.txt");
         Order.setOrders(OrderStorage.loadOrdersFromFile());
         new LoginRegisterView();
+        //comment :)
     }
 }
